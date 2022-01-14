7c1012414352ec0fa9f06464e277c80ea5ac20b9
samui <samui@hoodown.com>
2022-01-14 12:36:20 +0100

    more cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
-import 'dart:io';
-import 'dart:typed_data';
-import 'dart:async';
-
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:receive_sharing_intent/receive_sharing_intent.dart';
-//import 'package:interzone/moderator/xxCache.dart';
 
 import 'Provider/MultiSiteModeratorEntrySetProvider.dart';
 import 'Provider/SitesProvider.dart';
-import 'Provider/xxCacheProvider.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 import 'package:interzone/world.dart';
 import 'package:interzone/ModeratorSites.dart';
 
-//import 'views/multipleEntryCard.dart';
-//import 'views/ModeratorViewSettingsDefault.dart';
 import 'navigation/NavWrapper.dart';
 import '/sites/omasomeLoc.dart';
 
@@ -44,8 +34,6 @@ void main() async {
       ChangeNotifierProvider(
           create: (_) =>
               MultiSiteModeratorEntrySetProvider(sites.latestThreadsEntrySet)),
-      //ChangeNotifierProvider(
-      //    create: (_) => xxCacheProvider(iz.xxC, iz.baseUrl)),
       ChangeNotifierProvider(
           create: (_) => BottomNavigationBarProvider('lomba')),
     ],
@@ -54,26 +42,10 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  //final BottomNavigationBarProvider navi;
-  /*StreamSubscription _intentDataStreamSubscription =
-      ReceiveSharingIntent.getMediaStream().listen(
-          (List<SharedMediaFile> value) {
-    /*setState(() {
-        print("Shared:" + (_sharedFiles?.map((f)=> f.path)?.join(",") ?? ""));
-        _sharedFiles = value;
-      });*/
-  }, onError: (err) {
-    print("getIntentDataStream error: $err");
-  });*/
-
   // For sharing images coming from outside the app while the app is closed
   //  https://github.com/KasemJaffer/receive_sharing_intent
 
   MyApp();
-  //final Widget? homePage;
-
-  //final navi = BottomNavigationBarProvider();
 
   @override
   Widget build(BuildContext context) {
@@ -87,17 +59,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-/*class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Country',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: CombinationPage(),
-    );
-  }
-}*/
--- lib/views/AtacamaProfilePage.dart
+++ lib/views/AtacamaProfilePage.dart
@@ -1,6 +1,4 @@
 import 'dart:typed_data';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:keyboard_actions/keyboard_actions.dart';
 
@@ -28,13 +26,6 @@ class _AtacamaProfilePageState extends State<AtacamaProfilePage> {
     XFile? _pickedFile;
 
     return Scaffold(
-      /*floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera),
-        
-      ),*/
-      /*appBar: AppBar(
-        title: Text("welcome to " + world.appSplashImage),
-      ),*/
       body: Padding(
         padding: const EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0),
         child: Center(
@@ -63,40 +54,11 @@ class _AtacamaProfilePageState extends State<AtacamaProfilePage> {
                 children: [
                   SizedBox(
                     height: size.height / 1.6,
-                    child: /*IconButton(
-                      icon: const Icon(Icons.camera),
-                      tooltip: 'Attach a profile picture',
-                      onPressed: () async {
-                        try {
-                          final pickedFile = await _picker.pickImage(
-                            source: ImageSource.gallery,
-                            maxWidth: 640,
-                            maxHeight: 640,
-                            imageQuality: 72,
-                          );
-                          if (pickedFile != null) {
-                            //navi.setUploadImage(pickedFile);
-                            _pickedFile = pickedFile;
-                            //print(pickedFile.name);
-                          }
-                        } catch (e) {}
-
-                        //_focusNodeName.requestFocus();
-                      },
-                    ),*/
-                        Ink.image(
+                    child: Ink.image(
                       image: AssetImage('assets/moderatorSplash.jpeg'),
-                      //'assets/' + world.appSplashImage + '.jpeg'),
                       fit: BoxFit.fitWidth,
                     ),
                   ),
-
-                  /*TextField(
-                    focusNode: _focusNodeName,
-                    decoration: InputDecoration(
-                      labelText: "Product Name",
-                    ),
-                  ),*/
                   TextFormField(
                     controller: myTextEditingController,
                     maxLength: 10,
@@ -105,9 +67,6 @@ class _AtacamaProfilePageState extends State<AtacamaProfilePage> {
                     decoration: InputDecoration(
                       labelText: "add your user handle here",
                     ),
-
-                    //initialValue: 'guest', //_currentNick,
-                    //initialValue: navi.postingKeyboardTextFormFieldSeed,
                     onEditingComplete: () {
                       print('edco');
                     },
@@ -121,21 +80,16 @@ class _AtacamaProfilePageState extends State<AtacamaProfilePage> {
                       "I'm ready to go!",
                       style: TextStyle(color: Colors.white),
                     ),
-
-                    //color: Colors.green,
                     onPressed: () async {
                       Uint8List upfile = Uint8List(0);
                       if (_pickedFile != null) {
                         upfile = await _pickedFile.readAsBytes();
                       }
-                      //print('did');
-                      //print(myTextEditingController.text);
                       if (myTextEditingController.text.isNotEmpty) {
                         sites.setNick(myTextEditingController.text, '');
                       }
                       //todo clean this page
                       navi.closeAtacamaProfilePage();
-                      //navi.signal(AtacamaAction.viewAuthorThreads, '');
                     },
                   ),
                 ],
